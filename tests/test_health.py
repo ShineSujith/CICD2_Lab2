92958e1e9543600e1765f0a2cfdf5ccefdb3be73
Shine Sujith <g00414454@atu.ie>
2025-10-01 19:20:46 +0100

    added some extra comments

--- tests/test_health.py
+++ tests/test_health.py
@@ -3,6 +3,7 @@
 from conftests import client
 
 def test_health(client: client):
+    """tests health endpoint returns correct dict and status code on success"""
     result = client.get("/api/health")
     assert result.status_code == 200
     assert result.json() == {"status" : "ok"}
