77077c72ca8cbf28671c51506a1d2e3ac39acaa0
Shine Sujith <g00414454@atu.ie>
2025-10-06 13:06:42 +0100

    addressed comments

--- tests/test_health.py
+++ tests/test_health.py
@@ -1,8 +1,6 @@
 #tests/test_health.py
 
-from conftests import client
-
-def test_health(client: client):
+def test_health(client):
     """tests health endpoint returns correct dict and status code on success"""
     result = client.get("/api/health")
     assert result.status_code == 200
